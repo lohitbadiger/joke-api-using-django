83b472f67662e91e0119206fa4d4ff904a3bd282
Your Name <lohit2013n@gmail.com>
2019-03-28 12:15:19 +0530

    second

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from . models import Contact
+import requests
+import json
 # Create your views here.
 def home(request):
-    return render(request, 'mysite/home.html')
+    if request.method=='POST':
+        firstname=request.POST.get('fname')
+        lastname=request.POST.get('lname')
+
+        anything=requests.get('http://api.icndb.com/jokes/random?firstName='+ firstname + '&lastName='+ lastname)
+        json_data=json.loads(anything.text)
+        # print(anything.text)
+        # print(json_data)
+        joke=json_data.get('value').get('joke')
+        # print(joke)
+        context={
+            'joker': joke
+        }
+
+        return render(request, 'mysite/home.html', context)
+
+    else:
+        firstname='lohit'
+        lastname='badiger'
+        anything=requests.get('http://api.icndb.com/jokes/random?firstName='+ firstname + '&lastName='+ lastname)
+        json_data=json.loads(anything.text)
+        # print(anything.text)
+        # print(json_data)
+        joke=json_data.get('value').get('joke')
+        # print(joke)
+        context={
+            'joker': joke
+        }
+
+        return render(request, 'mysite/home.html', context)
+
 
 def portfolio(request):
     return render(request, 'mysite/portfolio.html')
